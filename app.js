bfc29869b56a9b98a025016546417f54eb7e87ec
therandomfool <mddenver@protonmail.com>
2020-11-10 22:27:05 -0800

    Adding card/deck logic

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var cards = [];
+var playerCard= [];
+var dealerCard = [];
+var cardCount = 0;
+
 var suits = ["spades", "hearts", "clubs", "diams"];
 var numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var output = document.getElementById("output");
+var dealerHolder = document.getElementById("dealerHolder");
+var dealerHolder = document.getElementById("playerHolder");
+
 for (s in suits) {
     var suit = suits[s][0].toUpperCase();
     var bgcolor = (suit == "S" || suit == "C") ? "black" : "red";
@@ -19,10 +26,37 @@ for (s in suits) {
     }
 }
 
-console.log(cards);
+
 
 function Start() {
-    var randomNum = Math.floor((Math.random() * 52));
-    output.innerHTML += "<span style='color:" + cards[randomNum].bgcolor + "'>&" + cards[randomNum].icon + ";" + cards[randomNum].cardnum + "</span>  ";
+    
+    shuffleDeck(cards);
+    dealNew();
+    outputCard();
 }
+
+function dealNew(){
+    playerCard = [];
+    dealerCard = [];
+    dealerHolder.innerHTML = "";
+    playerHolder.innerHTML = "";
+    
+}
+
+
+function shuffleDeck(array){
+    for(var i = array.length -1; i > 0; i--){
+        var j = Math.floor(Math.random() * (i+1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+
+}
+
+function outputCard(){
+    output.innerHTML += "<span style='color:" + cards[cardCount].bgcolor + "'>" + cards[cardCount].cardnum + "&" + cards[cardCount].icon + ";</span>  ";
+}
+
 ranCard();
